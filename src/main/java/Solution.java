7215e6231e6b6f312cf23867a625c286cc76492b
alek <wait82985@gmail.com>
2023-06-27 14:11:28 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,42 @@
+import java.util.Arrays;
+
 public class Solution {
     public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        if (n != 1) {
+            int index1 = -1;
+            for (int i = n - 1; i > 0; i--) {
+                if (nums[i - 1] < nums[i]) {
+                    index1 = i - 1;
+                    break;
+                }
+            }
+
+            if (index1 == -1) {
+                Arrays.sort(nums);
+            } else {
+                int index2 = -1;
+                for (int i = n - 1; i > index1; i--) {
+                    if (nums[i] > nums[index1]) {
+                        index2 = i;
+                        break;
+                    }
+                }
+
+                int temp = nums[index1];
+                nums[index1] = nums[index2];
+                nums[index2] = temp;
+                int[] tempArray = new int[n - index1 - 1];
+                for (int i = index1 + 1, j = 0; i < n; i++, j++) {
+                    tempArray[j] = nums[i];
+                }
+
+                for (int i = n - 1, j = 0; i > index1; i--, j++) {
+                    nums[i] = tempArray[j];
+                }
+            }
 
 
+        }
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -28,4 +28,22 @@ public class SolutionTest {
 
         Assert.assertArrayEquals(expected, nums);
     }
+
+    @Test
+    public void nextPermutationTest4() {
+        int[] nums = {1, 3, 2};
+        int[] expected = {2, 1, 3};
+        new Solution().nextPermutation(nums);
+
+        Assert.assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void nextPermutationTest5() {
+        int[] nums = {1, 3, 5, 4, 2};
+        int[] expected = {1, 4, 2, 3, 5};
+        new Solution().nextPermutation(nums);
+
+        Assert.assertArrayEquals(expected, nums);
+    }
 }
